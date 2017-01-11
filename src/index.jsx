b6eb4ff1ce8a678a5d8fbaee7464baac7e3149b3
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-01-11 16:49:08 +0800

    Moving the clock into function

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let hihi = 'gg';
+// let hihi = 'gg';
 
-ReactDOM.render(
-  <h1>Hello, world! {hihi}</h1>,
-  document.getElementById('root')
-);
+let clock = () => <div>Current time is {new Date().toLocaleTimeString()}</div>;
 
-let tick = () => {
-  console.log('gg');
-  const element = (
-    <div>Current time is {new Date().toLocaleTimeString()}</div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+let update = () => {
+  ReactDOM.render(clock(), document.getElementById('root'));
 };
 
-setInterval(tick, 1000);
+// ReactDOM.render(
+//   <h1>Hello, world! {hihi}</h1>,
+//   document.getElementById('root')
+// );
+//
+// let tick = () => {
+//   console.log('gg');
+//   const element = (
+//     <div>Current time is {new Date().toLocaleTimeString()}</div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// };
+
+setInterval(update, 1000);
